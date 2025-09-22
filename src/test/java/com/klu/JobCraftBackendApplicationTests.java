46591a42b1aaa3bc63c9e4039511780faebaeae8
AnithaEdara <2300031494@kluniversity.in>
2025-09-22 23:13:15 +0530

    first commit

--- src/test/java/com/klu/JobCraftBackendApplicationTests.java
+++ src/test/java/com/klu/JobCraftBackendApplicationTests.java
@@ -3,11 +3,11 @@ package com.klu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {}) // Avoid full context load
 class JobCraftBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
